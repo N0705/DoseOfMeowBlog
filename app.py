762cf1d2003b89c9f61e0649ff79d967a8479c1b
Mine Uddin <mineuddin@mines-air.mynetworksettings.com>
2024-02-04 12:22:14 -0500

    updated

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from init import create_app, create_database
 
 if __name__ == "__main__":
    app = create_app()
- #  app.run(debug=True)
-   app.run(host="192.168.1.160",port=5500)
+   app.run(debug=True)
+   #app.run(host="192.168.1.160",port=5500)
 
 #app = create_app()
 #ptcreate_database(app)
